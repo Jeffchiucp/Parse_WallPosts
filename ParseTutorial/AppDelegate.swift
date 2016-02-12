510888ba436e8097a2afa9bb0ee181d6106e688b
jeff <jchiu@cloudpassage.com>
2016-02-12 14:29:43 -0800

    update

--- ParseTutorial/AppDelegate.swift
+++ ParseTutorial/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -32,20 +33,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        println("Error: \(error) \(error.userInfo!)")
 //      }
 //    }
-    
-//    let query = PFQuery(className: "Player") //1
-//    query.whereKey("Name", equalTo: "John") //2
-//    query.whereKey("Score", greaterThan: 1000) //3
-//    query.findObjectsInBackgroundWithBlock { (objects, error) -> Void in //4
-//      if error == nil {
-//        println("Successfully retrieved: \(objects)")
-//      } else {
-//        println("Error: \(error) \(error.userInfo!)")
-//      }
-//    }
+
     
     return true
   }
+  
+  
+  func addTestData() {
+//    guard let entity = NSEntityDescription.entityForName("file", inManagedObjectContext: managedObjectContext), personEntity = NSEntityDescription.entityForName("file", inManagedObjectContext: managedObjectContext) else {
+      fatalError("Could not find file descriptions!")
+    }
+
+
   
   func applicationWillResignActive(application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -67,8 +66,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  
   }
   
+  // MARK: - Core Data Saving support
   
-}
+  
+  lazy var managedObjectContext: NSManagedObjectContext = {
+    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+//    let coordinator = self.persistentStoreCoordinator
+    var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+//    managedObjectContext.persistentStoreCoordinator = coordinator
+    return managedObjectContext
+  }()
+  
+  func saveContext () {
+//    if managedObjectContext.hasChanges {
+      do {
+//        try managedObjectContext.save()
+      } catch {
+        // Replace this implementation with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        let nserror = error as NSError
+        NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+        abort()
+      }
+    }
+  }
+  
+
 
